40f75bdce3fe89494ae625ad15988d14305da8be
Max <tool13@mail.ru>
2020-06-21 22:01:39 +0300

    fix height

--- src/scripts/fix_height.js
+++ src/scripts/fix_height.js
@@ -1,4 +1,7 @@
-export function setFullHeight() {
-    let vh = window.innerHeight;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
